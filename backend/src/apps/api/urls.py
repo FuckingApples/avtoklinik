4e972e197759f1ae6dfa6f412b1fc05c4c971a90
Aleksandr Zagrivnyy <zagrivnyj.af@edu.spbstu.ru>
2025-02-12 02:53:52 +0300

    feat: add api routes

--- backend/src/apps/api/urls.py
+++ backend/src/apps/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from apps.api.v1.users import UserViewSet
+from apps.api.v1.users import RegisterAPI
 
 router = DefaultRouter()
-router.register('users', UserViewSet, basename='users')
+# router.register("users", UserViewSet, basename="users")
 urlpatterns = [
-    path('v1/', include(router.urls)),
+    path("v1/", include(router.urls)),
+    path("v1/users/register", RegisterAPI.as_view(), name="register"),
 ]
