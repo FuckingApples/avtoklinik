1d6b48036d0b7aaa6d7768244ab6d703fb56c310
Aleksandr Zagrivnyy <zagrivnyj.af@edu.spbstu.ru>
2025-02-12 02:47:44 +0300

    feat: update serializer and add DTO class

--- backend/src/apps/api/serializers/users.py
+++ backend/src/apps/api/serializers/users.py
@@ -1,6 +1,38 @@
 from rest_framework import serializers
+from apps.users.models import User
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = "__all__"
+if TYPE_CHECKING:
+    from apps.users.models import User
+
+
+@dataclass
+class UserDTO:
+    first_name: str
+    last_name: str
+    email: str
+    password: str = None
+    id: int = None
+
+    @classmethod
+    def from_instance(cls, user: "User") -> "UserDTO":
+        return cls(
+            first_name=user.first_name,
+            last_name=user.last_name,
+            email=user.email,
+            id=user.id,
+        )
+
+
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def to_internal_value(self, data):
+        data = super().to_internal_value(data)
+
+        return UserDTO(**data)
