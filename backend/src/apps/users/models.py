c9e45d2179a5a1f0799bced1146febc289a4a1c2
Aleksandr Zagrivnyy <zagrivnyj.af@edu.spbstu.ru>
2025-02-12 02:40:17 +0300

    feat: add custom user model

--- backend/src/apps/users/models.py
+++ backend/src/apps/users/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(
+        self, email, first_name, last_name, password=None, **extra_fields
+    ) -> "User":
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not first_name:
+            raise ValueError("Users must have a first name")
+        if not last_name:
+            raise ValueError("Users must have a last name")
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(
+        self, email, first_name, last_name, password=None, **extra_fields
+    ) -> "User":
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+
+        user = self.create_user(email, first_name, last_name, password, **extra_fields)
+        user.save()
+        return user
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    username = None
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
