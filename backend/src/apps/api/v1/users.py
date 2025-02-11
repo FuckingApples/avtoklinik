d6c8c885072305859cec2e792e55ad3e9ef787c8
Aleksandr Zagrivnyy <zagrivnyj.af@edu.spbstu.ru>
2025-02-12 02:53:29 +0300

    feat: add user registration api endpoint

--- backend/src/apps/api/v1/users.py
+++ backend/src/apps/api/v1/users.py
@@ -1,6 +1,16 @@
-from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from apps.api.serializers.users import UserSerializer
+from apps.users.services import create
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+
+class RegisterAPI(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        data = serializer.validated_data
+        print(data)
+        serializer.instance = create.create_user(user=data)
+
+        return Response(data=serializer.data)
