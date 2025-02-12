308e183d2fbc4f2230d4b116ea4beb8f2fa40624
Aleksandr Zagrivnyy <zagrivnyj.af@edu.spbstu.ru>
2025-02-12 17:18:02 +0300

    fix: resolve permissions error

--- backend/src/apps/api/v1/users.py
+++ backend/src/apps/api/v1/users.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from apps.api.serializers.users import UserSerializer
@@ -5,12 +6,13 @@ from apps.users.services import create
 
 
 class RegisterAPI(APIView):
+    permission_classes = (AllowAny,)
+
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
         data = serializer.validated_data
-        print(data)
         serializer.instance = create.create_user(user=data)
 
         return Response(data=serializer.data)
