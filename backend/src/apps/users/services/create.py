f2967127ec8adbc828d1eb26ed867e57dae1c58d
Aleksandr Zagrivnyy <zagrivnyj.af@edu.spbstu.ru>
2025-02-12 17:17:25 +0300

    fix: check if user already exists and rise error on conflict

--- backend/src/apps/users/services/create.py
+++ backend/src/apps/users/services/create.py
@@ -1,15 +1,24 @@
+from django.db import IntegrityError
+from rest_framework.exceptions import ValidationError
+
 from apps.api.serializers.users import UserDTO
 from apps.users.models import User
 
 
 def create_user(user: "UserDTO") -> "UserDTO":
-    instance = User(
-        first_name=user.first_name,
-        last_name=user.last_name,
-        email=user.email,
-    )
+    if User.objects.filter(email=user.email).exists():
+        raise ValidationError("User already exists")
 
-    if user.password:
-        instance.set_password(user.password)
-    instance.save()
-    return UserDTO.from_instance(instance)
+    try:
+        instance = User(
+            first_name=user.first_name,
+            last_name=user.last_name,
+            email=user.email,
+        )
+
+        if user.password:
+            instance.set_password(user.password)
+        instance.save()
+        return UserDTO.from_instance(instance)
+    except IntegrityError:
+        raise ValidationError({"email": "This email is already in use."})
