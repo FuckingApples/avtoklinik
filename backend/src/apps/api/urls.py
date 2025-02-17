f9db39f5266d28244ffe7f26ad8bf75eda6665d9
Aleksandr Zagrivnyy <zagrivnyj.af@edu.spbstu.ru>
2025-02-18 02:34:51 +0300

    feat: add new endpoints urls

--- backend/src/apps/api/urls.py
+++ backend/src/apps/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from apps.api.v1.tokens import CustomTokenObtainPairView, CustomTokenRefreshView
+from apps.api.v1.otp import RequestEmailOTPAPI, VerifyEmailOTPAPI
+from apps.api.v1.tokens import CustomTokenObtainPairAPI, CustomTokenRefreshAPI
 from apps.api.v1.users import RegisterAPI
 
 router = DefaultRouter()
@@ -9,6 +10,8 @@ router = DefaultRouter()
 urlpatterns = [
     path("v1/", include(router.urls)),
     path("v1/users/register", RegisterAPI.as_view(), name="register"),
-    path("token", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh", CustomTokenRefreshView.as_view(), name="token_refresh"),
+    path("v1/email/verify", VerifyEmailOTPAPI.as_view(), name="verify_email"),
+    path("v1/email/verify/get_otp", RequestEmailOTPAPI.as_view(), name="get_email_otp"),
+    path("token", CustomTokenObtainPairAPI.as_view(), name="token_obtain_pair"),
+    path("token/refresh", CustomTokenRefreshAPI.as_view(), name="token_refresh"),
 ]
