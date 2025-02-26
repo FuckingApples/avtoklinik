27385b78987cbf3f77455537a3dd68b5aee04c92
Aleksandr Zagrivnyy <zagrivnyj.af@edu.spbstu.ru>
2025-02-27 01:04:56 +0300

    feat: update components to match new tailwindcss and react versions

--- frontend/src/components/ui/input.tsx
+++ frontend/src/components/ui/input.tsx
@@ -6,38 +6,38 @@ export type InputProps = React.ComponentProps<"input"> & {
   endIcon?: React.JSX.Element;
 };
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  (
-    { className, type, startIcon: StartIcon, endIcon: EndIcon, ...props },
-    ref,
-  ) => {
-    return (
-      <div className="relative">
-        {StartIcon && (
-          <div className="pointer-events-none absolute inset-0 left-0 flex items-center pl-3">
-            {StartIcon}
-          </div>
+const Input = ({
+  className,
+  type,
+  startIcon: StartIcon,
+  endIcon: EndIcon,
+  ...props
+}: InputProps) => {
+  return (
+    <div data-slot="input" className="relative">
+      {StartIcon && (
+        <div className="pointer-events-none absolute inset-0 left-0 flex items-center pl-3">
+          {StartIcon}
+        </div>
+      )}
+      <input
+        type={type}
+        className={cn(
+          "border-input bg-background ring-offset-background file:text-foreground placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-base file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-hidden disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          StartIcon && "pl-10",
+          EndIcon && "pr-10",
+          className,
         )}
-        <input
-          type={type}
-          className={cn(
-            "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-            StartIcon && "pl-10",
-            EndIcon && "pr-10",
-            className,
-          )}
-          ref={ref}
-          {...props}
-        />
-        {EndIcon && (
-          <div className="pointer-events-none absolute right-0 top-1/2 flex -translate-y-1/2 items-center pr-3">
-            {EndIcon}
-          </div>
-        )}
-      </div>
-    );
-  },
-);
+        {...props}
+      />
+      {EndIcon && (
+        <div className="pointer-events-none absolute top-1/2 right-0 flex -translate-y-1/2 items-center pr-3">
+          {EndIcon}
+        </div>
+      )}
+    </div>
+  );
+};
 
 Input.displayName = "Input";
 export { Input };
--- frontend/src/components/ui/label.tsx
+++ frontend/src/components/ui/label.tsx
@@ -4,20 +4,20 @@ import React from "react";
 import { Label as LabelPrimitive } from "radix-ui";
 import { cn } from "~/lib/utils";
 
-const Label = React.forwardRef<
-  React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root
-    className={cn(
-      "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
-      className,
-    )}
-    ref={ref}
-    {...props}
-  />
-));
-
-Label.displayName = LabelPrimitive.Root.displayName;
+function Label({
+  className,
+  ...props
+}: React.ComponentProps<typeof LabelPrimitive.Root>) {
+  return (
+    <LabelPrimitive.Root
+      data-slot="label"
+      className={cn(
+        "text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
 
 export { Label };
