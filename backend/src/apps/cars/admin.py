cc58fcf82160aae53132bbf501ea07fcd0a4d20a
Aleksandr Zagrivnyy <zagrivnyj.af@edu.spbstu.ru>
2025-03-07 19:30:54 +0300

    feat: add car model and serializer
    Co-authored-by: Mertguines <136751233+mertguines@users.noreply.github.com>

---
+++ backend/src/apps/cars/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+
+from apps.cars.models import Car
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = (
+        "vin",
+        "__str__",
+        "mileage",
+        "license_plate",
+        "license_plate_region",
+    )
+    search_fields = ("license_plate", "vin", "model", "brand")
+    list_filter = ("license_plate_region", "year")
+    date_hierarchy = "created_at"
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "vin",
+                    "year",
+                    "mileage",
+                    ("brand", "model"),
+                    ("license_plate", "license_plate_region"),
+                ),
+            },
+        ),
+    )
