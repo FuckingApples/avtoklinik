4e4071cb548c96c6e22fe9c564a2228a886ee3f2
Aleksandr Zagrivnyy <zagrivnyj.af@edu.spbstu.ru>
2025-03-14 16:03:09 +0300

    hotfix: add foreign key for client

--- backend/src/apps/clients/models.py
+++ backend/src/apps/clients/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from apps.organizations.models import Organization
 
 
 class Client(models.Model):
@@ -8,6 +9,7 @@ class Client(models.Model):
     phone = models.CharField(max_length=12, unique=True)
     email = models.EmailField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    organization = models.ForeignKey(Organization, on_delete=models.PROTECT)
 
     def __str__(self):
         return f"{self.last_name} {self.first_name} {self.middle_name if self.middle_name else ''}"
