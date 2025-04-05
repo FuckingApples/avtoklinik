552a3992eb9c506d95ede4eac17e9120e988ba98
MayaSakovskaya <147191353+MayaSakovskaya@users.noreply.github.com>
2025-04-06 01:14:36 +0300

    trying to add requests model

--- backend/src/apps/api/v1/urls.py
+++ backend/src/apps/api/v1/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from apps.api.v1.oauth import OAuthProviderViewSet
+from apps.api.v1.requests import OrganizationRequestsAPI, RequestAPI
 
 router = DefaultRouter()
 router.register("oauth", OAuthProviderViewSet, basename="oauth")
@@ -10,4 +11,6 @@ urlpatterns = [
     path("deals/", include("apps.api.v1.deals")),
     path("registries/", include("apps.api.v1.registries")),
     path("", include(router.urls)),
+path("<int:organization_id>/requests/", OrganizationRequestsAPI.as_view(), name="organization_requests"),
+    path("<int:organization_id>/requests/<int:request_id>/", RequestAPI.as_view(), name="request_detail"),
 ]
