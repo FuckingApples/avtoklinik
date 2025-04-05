b90b8923228261d5b5ef1ef7f5cb9372e4320b07
neefee <contact@neefee.ru>
2025-04-05 21:08:00 +0300

    [feat] added base model: SoftDeleteModel

--- backend/src/apps/core/models.py
+++ backend/src/apps/core/models.py
@@ -1,6 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
 
 class SafeDeleteManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(is_deleted=False)
+
+
+class SoftDeleteModel(models.Model):
+    is_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+    def soft_delete(self):
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save()
+
+    def restore(self):
+        self.is_deleted = False
+        self.deleted_at = None
+        self.save()
