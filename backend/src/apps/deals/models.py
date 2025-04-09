e912543a13c5faa3ba157813ec8ac673a1e19606
neefee <contact@neefee.ru>
2025-04-09 23:26:52 +0300

    [refactor] request model moved to deals app

--- backend/src/apps/deals/models.py
+++ backend/src/apps/deals/models.py
@@ -20,3 +20,27 @@ class Deal(SoftDeleteModel):
 
     def __str__(self):
         return f"Deal № {self.number} (Organization: {self.organization}, Client: {self.client})"
+
+
+class ClientRequest(models.Model):
+    number = models.CharField(max_length=25)
+    organization = models.ForeignKey(
+        "organizations.Organization", on_delete=models.CASCADE, related_name="requests"
+    )
+    deal = models.ForeignKey(
+        Deal, on_delete=models.SET_NULL, null=True, blank=True, related_name="requests"
+    )
+    employee = models.ForeignKey(
+        "users.User", on_delete=models.SET_NULL, null=True, blank=True
+    )
+    workplace = models.ForeignKey(
+        "registries.Workplace", on_delete=models.SET_NULL, null=True
+    )
+    date_start = models.DateTimeField()
+    date_end = models.DateTimeField()
+    comment = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Request № {self.number} (Organization: {self.organization.name})"
