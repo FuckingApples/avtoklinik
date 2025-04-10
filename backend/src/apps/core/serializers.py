576ee284af8dab3917dc888a219712f3837351bf
neefee <contact@neefee.ru>
2025-04-10 03:24:19 +0300

    [feat] updated BaseOrganizationModelSerializer, deleted old request serializer

--- backend/src/apps/core/serializers.py
+++ backend/src/apps/core/serializers.py
@@ -2,7 +2,12 @@ from rest_framework import serializers
 
 
 class BaseOrganizationModelSerializer(serializers.ModelSerializer):
+    fields_to_delete = []
+
     def create(self, validated_data):
+        for field in self.fields_to_delete:
+            validated_data.pop(field, None)
+
         organization = self.context.get("organization")
         if organization:
             validated_data["organization"] = organization
